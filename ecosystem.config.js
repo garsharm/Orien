8c58ace59177a6ebf34adaf344a9a0882dfb9d7e
Garvit <grvtsharma.sharma@gmail.com>
2018-08-07 22:33:42 +0530

    PM2 ecosysfile

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [{
+    name      : 'API',
+    script    : 'index.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      key  : 'c:/starserver.pem',
+      host : 'ec2-13-58-82-111.us-east-2.compute.amazonaws.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:garsharm/Orien.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
